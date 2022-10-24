a2b9d2c8d1cd56883ed70082412cdff87a013352
SebastianDD <43751982+SebastianDD@users.noreply.github.com>
2022-10-24 16:30:44 +0200

    apply requested changes
    Co-authored-by: Sasan Jacob Rasti <sasan_jacob.rasti@tu-dresden.de>

--- powerfactory_utils/exporter/exporter.py
+++ powerfactory_utils/exporter/exporter.py
@@ -524,7 +524,7 @@ class PowerfactoryExporter:
 
                 r1 = l_type.rline * line.dline / line.nlnum * Exponents.RESISTANCE
                 x1 = l_type.xline * line.dline / line.nlnum *Exponents.REACTANCE
-                r0 = l_type.rline0 * line.dline / line.nlnum *Exponents.RESISTANCE
+                r0 = l_type.rline0 * line.dline / line.nlnum * Exponents.RESISTANCE
                 x0 = l_type.xline0 * line.dline / line.nlnum *Exponents.REACTANCE
                 g1 = l_type.gline * line.dline * line.nlnum *Exponents.CONDUCTANCE
                 b1 = l_type.bline * line.dline * line.nlnum * Exponents.SUSCEPTANCE
