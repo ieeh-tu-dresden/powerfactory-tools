7301c999bbbf15f1221ad0197f6846c8f8dcc714
Sasan Jacob Rasti <sasan_jacob.rasti@tu-dresden.de>
2023-02-14 15:53:51 +0100

    remove shebangs

--- powerfactory_tools/schema/topology/node.py
+++ powerfactory_tools/schema/topology/node.py
@@ -1,4 +1,3 @@
-#! /usr/bin/python
 # :author: Sasan Jacob Rasti <sasan_jacob.rasti@tu-dresden.de>
 # :copyright: Copyright (c) Institute of Electrical Power Systems and High Voltage Engineering - TU Dresden, 2022-2023.
 # :license: BSD 3-Clause
