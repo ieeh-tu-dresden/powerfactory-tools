9c5fad3bff0be126552530b69de2174c5be35e39
krahmers <sebastian.krahmer@tu-dresden.de>
2023-02-15 18:50:32 +0100

    replace pfd example grid and add author contributions

--- powerfactory_tools/constants.py
+++ powerfactory_tools/constants.py
@@ -1,4 +1,5 @@
 # :author: Sasan Jacob Rasti <sasan_jacob.rasti@tu-dresden.de>
+# :author: Sebastian Krahmer <sebastian.krahmer@tu-dresden.de>
 # :copyright: Copyright (c) Institute of Electrical Power Systems and High Voltage Engineering - TU Dresden, 2022-2023.
 # :license: BSD 3-Clause
 
