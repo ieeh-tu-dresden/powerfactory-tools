fe912fa305a56b2470e9183798b41b8b46ffe9dc
Sasan Jacob Rasti <sasan_jacob.rasti@tu-dresden.de>
2023-03-21 10:22:55 +0100

    add simple interface test

---
+++ tests/test_interface.py
@@ -0,0 +1,15 @@
+import logging
+
+from powerfactory_tools.interface import PowerFactoryInterface
+
+
+class TestInterface:
+    def test_init(self, caplog):
+        with caplog.at_level(logging.INFO):
+            PowerFactoryInterface(
+                project_name="test",
+            )
+
+        assert "Could not start PowerFactory Interface. Shutting down..." in caplog.text
+        assert "Closing PowerFactory Interface..." in caplog.text
+        assert "Closing PowerFactory Interface... Done." in caplog.text
