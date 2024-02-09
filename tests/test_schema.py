44f0e476b026a836589186895d369938c3ea5d92
SebastianDD <43751982+SebastianDD@users.noreply.github.com>
2024-02-09 11:14:11 +0100

    fix: make json export more efficient (contributors: @SebastianDD)

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -36,12 +36,11 @@ def test_schema_import(case, schema_class, json_file_name) -> None:
     json_file_path = GRID_PATH / case / (case + "_HV_9_Bus_" + json_file_name)
 
     data = schema_class.from_file(json_file_path)
-    _json_str1 = data.model_dump_json()
-    _data = json.loads(_json_str1)
-    json_str1 = json.dumps(_data, sort_keys=True)
+    json_str1 = json.dumps(data.model_dump(mode="json"), indent=2, sort_keys=True)
 
     with json_file_path.open(encoding="utf-8") as file_handle:
         data = json.load(file_handle)
-    json_str2 = json.dumps(data, sort_keys=True)
+
+    json_str2 = json.dumps(data, indent=2, sort_keys=True)
 
     assert json_str1 == json_str2
